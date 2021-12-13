714f8d71e0ea86405ed56310989de7c635141f3c
brurya-e <brurya.zak@gmail.com>
2021-12-13 12:07:16 +0200

    4.1

---
+++ 4.1/4.1.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+function Buttons() {
+  return (
+    <div>
+<Button text ="Important" class ="important"/>
+<Button text ="not importent" class ="notImportant"/>
+
+</div>
+
+);
+}
+export default Buttons;
